8f9f22aea180c7c42a70cce3546ffde0fdcd308b
Andrei Dobrescu <usingvirtualbox@gmail.com>
2020-08-01 10:50:40 +0300

    project structure

---
+++ library/src/main/java/ro/andreidobrescu/slcd4a/Run.kt
@@ -0,0 +1,5 @@
+package ro.andreidobrescu.slcd4a
+
+class Run
+{
+}
---
+++ sample-java/src/main/java/ro/andreidobrescu/slcd4aj/Main.java
@@ -0,0 +1,9 @@
+package ro.andreidobrescu.slcd4aj;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello!");
+    }
+}
